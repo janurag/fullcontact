d640fbfdc8a07fadac199edb0024eeca503935d7
Anurag Jain <anurag.jain@innovaccer.com>
2015-07-24 13:44:12 +0000

    Interrupt handling done

--- contact.py
+++ contact.py
@@ -3,6 +3,8 @@ import json
 import csv
 import time
 from datetime import datetime, timedelta
+import os.path
+
 
 REQUEST_LATENCY = 0.2
 next_req_time  = datetime.fromtimestamp(0)
@@ -12,6 +14,47 @@ print "\nnext_req_time", next_req_time
 api_key = '891f653fab77f251'
 url = "https://api.fullcontact.com/v2/person.json"
 stats = {'facebook': 0, 'foursquare':0, 'twitter':0, 'googleplus':0}
+
+
+interrupt_row_number = 0
+x = os.path.isfile('interrupt_handle.csv')
+
+
+####  Interrupt Handling START #####
+if x!=True:
+	# create file and store count value
+	total_row_count = 0
+	with open('emails.csv', 'rb') as csvfile:
+		email_value = csv.reader(csvfile)
+		email_val = email_value
+		#print repr(email_value)
+		i = 1
+		row_count = sum(1 for t in email_value)
+		total_row_count = row_count/2
+		# print row_count
+		print total_row_count
+		f = open("interrupt_handle.csv", "w+")
+		fcsv = csv.writer(f, delimiter=",")
+		fcsv.writerow([total_row_count, 0])
+
+
+else:
+	pass
+	interrupt_handle = open('interrupt_handle.csv', 'r+')
+	int_handle_read = csv.reader(interrupt_handle)
+	for row in int_handle_read:
+		print repr(row) 
+		total_count = int(row[0])
+		interrupt_row_number = int(row[1])+3
+
+#### Interrupt Handling END ####
+
+
+
+
+
+
+
 
 ## function for requesting data
 def whois(email, api_key):
@@ -75,11 +118,18 @@ def update_rate_limit(hdr):
  #reading emails from a file
 with open('emails.csv', 'rb') as csvfile:
 	email_value = csv.reader(csvfile)
+	print "total_row_count:" , total_count
+
 	#print repr(email_value)
 	i = 1
 	for row in email_value:
 		#print 'loop'
 		if i%2==1:
+			print interrupt_row_number
+			if interrupt_row_number !=0:
+				interrupt_row_number-=1
+				continue
+
 			print row[0], "\n"
 			whois(row[0], api_key)
 
--- stats.py
+++ stats.py
@@ -1,45 +1,64 @@
 import json, csv
+import os.path
+
+interrupt_row_number = 0
+x = os.path.isfile('interrupt_handle.csv')
+
+
+####  Interrupt Handling START #####
+if x!=True:
+	# create file and store count value
+	total_row_count = 0
+	with open('emails.csv', 'rb') as csvfile:
+		email_value = csv.reader(csvfile)
+		email_val = email_value
+		#print repr(email_value)
+		i = 1
+		row_count = sum(1 for t in email_value)
+		total_row_count = row_count/2
+		# print row_count
+		print total_row_count
+		f = open("interrupt_handle.csv", "w+")
+		fcsv = csv.writer(f, delimiter=",")
+		fcsv.writerow([total_row_count, 0])
+
+
+else:
+	pass
+	interrupt_handle = open('interrupt_handle.csv', 'r+')
+	int_handle_read = csv.reader(interrupt_handle)
+	for row in int_handle_read:
+		print repr(row) 
+		total_count = int(row[0])
+		interrupt_row_number = int(row[1])+3
+
+#### Interrupt Handling END ####
 
 
 
- #reading emails from a file
+
+#reading emails from a file
 with open('emails.csv', 'rb') as csvfile:
 	email_value = csv.reader(csvfile)
-	email_val = email_value
-	#print repr(email_value)
-	i = 1
-	row_count = sum(1 for t in email_value)
-	total_row_count = row_count/2
-	print row_count
-	print total_row_count
-	# count = 0
-	# print repr(email_value)
-	# email_val = csv.reader(csvfile)
-	# print repr(email_value)
-	# count = 0
-	# for result in email_val:
-	# 	print 'loop'
-	# 	count +=1
-	# 	if i%2==1:
-	# 		print result[0], "\n"
-	# 		# whois(row[0], api_key)
-
-	# 	i+=1   # email value
-	# print count
-
-with open('emails.csv', 'rb') as csvfile:
-	email_value = csv.reader(csvfile)
+	print "total_row_count:" , total_count
 	count = 0
+	i = 1
 	for result in email_value:
-		# print 'loop'
 		count +=1
 		if i%2==1:
+			print interrupt_row_number
+			if interrupt_row_number !=0:
+				interrupt_row_number-=1
+				continue
+
 			pass
-			# print result[0], "\n"
+			print result[0], "\n"
+			if i==21:
+				break
 			# whois(row[0], api_key)
 
 		i+=1   # email value
-	print count
+
 
 
 
